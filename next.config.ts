fa67b6dd3dccc6ae0e74bc0cc827ac7fd05f823c
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-09-30 12:04:49 +0300

    Enable TypeScript to ignore build errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   typescript: {
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
