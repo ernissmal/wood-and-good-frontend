6955d1416b17d0800f0225f618ec4ff71830c1b0
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-10-01 15:21:40 +0300

    Add Sanity CLI configuration for project setup

---
+++ sanity.cli.ts
@@ -0,0 +1,11 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hrcndigj',
+    dataset: 'production'
+  },
+  deployment: {
+    appId: 'qy0g23xzlxb0rwl16i22bbev'
+  }
+})
