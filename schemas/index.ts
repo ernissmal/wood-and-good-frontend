2fd15544b607e007c6993c59947cdb4ad74767c1
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-10-01 15:50:39 +0300

    Refactor schema import order for improved organization and clarity

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,14 +7,32 @@ import productContent from './productContent'
 import blogPost from './blogPost'
 import testimonial from './testimonial'
 
+// Table configuration schemas
+import tableShape from './tableShape'
+import tableMaterial from './tableMaterial'
+import tableSize from './tableSize'
+import tableQuality from './tableQuality'
+import tableConfiguration from './tableConfiguration'
+import customerQuote from './customerQuote'
+
 export const schemaTypes = [
   // Categories (create these first as they're referenced by other types)
   productCategory,
   productType,
   blogCategory,
   
+  // Table configuration components
+  tableShape,
+  tableMaterial,
+  tableSize,
+  tableQuality,
+  
   // Content types that reference categories
   productContent,
   blogPost,
   testimonial,
+  
+  // Advanced table configuration and quotes
+  tableConfiguration,
+  customerQuote,
 ]
